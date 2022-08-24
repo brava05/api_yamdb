5a1aafe19b36550324e976623741a86f20c5f59b
Piton <brava_05@mail.ru>
2022-08-24 15:25:27 +0300

    permissions

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Review, Comment, Category
+from reviews.models import Review, Comment, Category, Genre, Title, TitleGenre
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -31,3 +31,15 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Title
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.throttling import AnonRateThrottle
 
-from reviews.models import Review, Comment, Category
+from reviews.models import Genre, Review, Comment, Category, Title
 from .serializers import ReviewSerializer, CommentSerializer, CategorySerializer
-
+from .serializers import TitleSerializer, GenreSerializer
+from .permissions import AdminOrReadOnly, AuthorAdminModeratorOrReadOnly
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    # permission_classes = (OwnerOrReadOnly,)
-    # throttle_classes = (AnonRateThrottle,)
+    permission_classes = (AuthorAdminModeratorOrReadOnly,)
+    throttle_classes = (AnonRateThrottle,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -17,7 +19,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    # permission_classes = (OwnerOrReadOnly,)
+    permission_classes = (AuthorAdminModeratorOrReadOnly,)
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
@@ -29,6 +31,19 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(review=review, author=self.request.user)
 
 
-class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (AdminOrReadOnly,)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (AdminOrReadOnly,)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (AdminOrReadOnly,)
