07f9b72de68f28d89ad9540000ce6edd328b9dc8
Piton <brava_05@mail.ru>
2022-08-28 19:10:05 +0300

    made title POST

--- api_yamdb/api/pagination.py
+++ api_yamdb/api/pagination.py
@@ -1,19 +1,19 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 
-class CustomPagination(LimitOffsetPagination):
-    page_size = 4
+class CustomPagination(PageNumberPagination):
+    page_size = 5
 
-    def get_paginated_response(self, data):
-        return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
-            },
-            'count': self.page.paginator.count,
-            'results': data
-        })
+    # def get_paginated_response(self, data):
+    #     return Response({
+    #         'links': {
+    #             'next': self.get_next_link(),
+    #             'previous': self.get_previous_link()
+    #         },
+    #         'count': self.page.paginator.count,
+    #         'results': data
+    #     })
 
 
 # class CustomCommentPagination(LimitOffsetPagination):
