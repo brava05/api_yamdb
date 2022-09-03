065a6806a94e8bea617dfd9b1d15d17f1a5da17d
Ruslan <glotovruslann@yandex.ru>
2022-09-03 22:50:48 +0300

    changed after second review

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,12 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-CHOICES = (
-    ('user', 'Аутентифицированный пользователь'),
-    ('moderator', 'Модератор'),
-    ('admin', 'Администратор'),
-)
-
 
 class User(AbstractUser):
 
